73304a1b050b775e25074e3b96a656802a4f4958
Brian <41770471+Brianleft28@users.noreply.github.com>
2024-02-21 00:34:46 -0300

    css last changed

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,6 +1,3 @@
-import imgCabecera from "../assets/img-cabecera.jpg";
 export const Contact = () => {
-  return (
-    <img src={imgCabecera} className='img-cabecera' alt='Imagen de Cabecera' />
-  );
+  return <div>Contact</div>;
 };
