18019b815901784e926c7a04fcd56eb2380f2a1b
Brian <41770471+Brianleft28@users.noreply.github.com>
2024-02-20 23:04:33 -0300

    home css changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,29 +9,32 @@ import sophia from "../assets/princesa.jpeg";
 import spiderman from "../assets/spiderman.jpg";
 import roblox from "../assets/roblox.jpeg";
 import niños from "../assets/niños.png";
-/* import { IoClose } from "react-icons/io5"; */
-
+import { IoClose } from "react-icons/io5";
+/* Experts img */
+import economia from "../assets/economia.png";
+import logistica from "../assets/logistica.png";
+import creatividad from "../assets/creatividad.png";
 export const Home = () => {
   return (
     <>
       {/* Head */}
       <header className='header' id='inicio'>
         <div className='contenedor head'>
-          <h1>Candy Pony</h1>
-          <p className='copy'>Convertimos tus sueños en realidad</p>
+          <h1 className='title'>Convertimos tus sueños en realidad</h1>
+          <p className='copy'>Servicio de evento Candy Pony</p>
         </div>
       </header>
       {/* Comienzo de Main */}
       <main>
         {/* Servicios */}
-        <section className='services contenedor' id='servicio'>
+        <sectcion className='contenedor' id='servicio'>
           <h2 className='subtitulo'>Nuestro Servicio</h2>
           <div className='contenedor-servicio'>
             <img src={niños} alt='Imagen de Niño en Cumpleaños animada' />
             <div className='checklist-servicio'>
               <div className='service'>
                 <h3 className='n-service'>
-                  <span className='number'>1</span>Eventos de cumpleaños
+                  <span className='number'>1</span> Eventos de cumpleaños
                 </h3>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
@@ -41,7 +44,7 @@ export const Home = () => {
               </div>
               <div className='service'>
                 <h3 className='n-service'>
-                  <span className='number'>2</span>Stand de Glitter
+                  <span className='number'>2</span> Stand de Glitter
                 </h3>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
@@ -51,7 +54,7 @@ export const Home = () => {
               </div>
               <div className='service'>
                 <h3 className='n-service'>
-                  <span className='number'>3</span>Sublimados
+                  <span className='number'>3</span> Sublimados
                 </h3>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo
@@ -75,25 +78,36 @@ export const Home = () => {
               </div>
             </div>
           </section>
-          {/*  <section className='imagen-light'>
-          <IoClose className='close' />
-          <img src='' alt='' className='agregar-imagen' />
-        </section> */}
+
+          <section className='imagen-light'>
+            <IoClose color='white' className='close' />
+            <img src={cars} alt='asd' className='agregar-imagen' />
+          </section>
+
           <section className='contenedor' id='expertos'>
             <h2 className='subtitulo'>Expertos en:</h2>
             <section className='experts'>
               <div className='cont-experts'>
-                <img src='' alt='' />
-                <h3 className='n-expert'>Creatividad</h3>
+                <img src={creatividad} alt='Logo de Creatividad' />
+                <h3 className='n-expert font-semibold'>Creatividad</h3>
+              </div>
+              <div className='cont-experts'>
+                <img src={logistica} alt='Logo de Logistica' />
+                <h3 className='n-expert font-semibold'>Logística</h3>
+              </div>
+              <div className='cont-experts'>
+                <img src={economia} alt='Logo de Economia' />
+                <h3 className='n-expert font-semibold'>Cuidar tu economia</h3>
               </div>
             </section>
           </section>
-        </section>
+        </sectcion>
       </main>
+
       <footer id='contacto'>
         <div className='contenedor footer-content'>
           <div className='contact-us'>
-            <h2 className='brand'>Candy Pony</h2>
+            <h2 className='brand font-semibold'>Candy Pony</h2>
             <p>Somos expertos en la creación de tu evento</p>
           </div>
           <div className='social-media'>
@@ -108,6 +122,7 @@ export const Home = () => {
             </a>
           </div>
         </div>
+        <div className='line'></div>
       </footer>
     </>
   );
